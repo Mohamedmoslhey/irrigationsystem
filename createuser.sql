2bdcc76bca8fa3e26a98cca820040faf2c051c65
Mohamedmoslhey <mem.moslhey@outlook.com>
2022-12-11 16:22:15 +0200

    Sql upload
    Sql upload

---
+++ createuser.sql
@@ -0,0 +1,3 @@
+CREATE USER 'irrigationuser'@'localhost' IDENTIFIED BY 'irrigationuser';
+
+GRANT ALL PRIVILEGES ON dbTest.* To 'irrigationuser'@'localhost' IDENTIFIED BY 'irrigationuser';
---
+++ irrigationsys3table.sql
@@ -0,0 +1,25 @@
+INSERT INTO `irrigationsys_info`.`irrigation_sensor`
+VALUES
+('1', 'SnsNo.1', '0', '10'),
+('2', 'SnsNo.2', '0', '15'),
+('3', 'SnsNo.3', '0', '20'),
+('4', 'SnsNo.4', '0', '25'),
+('5', 'SnsNo.5', '0', '30');
+
+INSERT INTO `irrigationsys_info`.`land_plots`
+VALUES
+('1', 'PltNo.1', 'Dry', '10', '1', '1'),
+('2', 'PltNo.2', 'Dry', '15', '2', '2'),
+('3', 'PltNo.3', 'Dry', '20', '3', '3'),
+('4', 'PltNo.4', 'Dry', '25', '4', '4'),
+('5', 'PltNo.5', 'Dry', '30', '5', '5');
+
+INSERT INTO `irrigationsys_info`.`land_timeandwateramount`
+VALUES
+('1','10','30','40'),
+('1','15','35','45'),
+('1','20','40','50'),
+('1','25','35','55'),
+('1','30','50','60');
+
+
